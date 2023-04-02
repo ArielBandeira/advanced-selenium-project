c334785b562529dcb632640b4e488009437ea5c2
ariel <arieltwain@gmail.com>
2023-04-02 18:29:47 -0300

    added Dropdown tests

--- src/test/java/com/herokuapp/theinternet/dropdowntests/DropdownTests.java
+++ src/test/java/com/herokuapp/theinternet/dropdowntests/DropdownTests.java
@@ -4,13 +4,15 @@ import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.DropdownPage;
 import com.herokuapp.theinternet.pages.WelcomePageObject;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class DropdownTests extends TestUtilities {
 
+    @Parameters({ "optionSelected" })
     @Test
-    public void optionTwoTest() {
-        log.info("Starting optionTwoTest");
+    public void selectOptionTest(Integer optionSelected) {
+        log.info("Starting selectOptionTest");
 
         //Open main page
         WelcomePageObject welcomePage = new WelcomePageObject(driver, log);
@@ -20,12 +22,11 @@ public class DropdownTests extends TestUtilities {
         DropdownPage dropdownPage = welcomePage.clickDropdownLink();
 
         //Select option 2
-        dropdownPage.selectOption(2);
+        dropdownPage.selectOption(optionSelected);
 
         //Verify if option 2 is selected
         String selectedOption = dropdownPage.getSelectedOption();
-        Assert.assertTrue(selectedOption.equals("Option 2"), "Option selected is not the expected");
-
+        Assert.assertTrue(selectedOption.equals("Option " + optionSelected), "Option selected is not the expected");
 
     }
 
