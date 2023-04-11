1d230c1502867b00c2f5302cb5fdcf3cc6ee74d0
ariel <arieltwain@gmail.com>
2023-04-10 23:46:43 -0300

    updating varius test cleaning up code

--- src/main/java/com/herokuapp/theinternet/pages/BasePageObject.java
+++ src/main/java/com/herokuapp/theinternet/pages/BasePageObject.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Listeners;
 
 import java.time.Duration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+@Listeners({ com.herokuapp.theinternet.base.TestListener.class })
 public class BasePageObject {
 
     protected WebDriver driver;
